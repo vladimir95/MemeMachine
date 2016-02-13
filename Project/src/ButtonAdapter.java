8db7aae64f7514740e56dac3f87c412bed57a5b4
vladimir95 <vladimirmartintsov95@gmail.com>
2016-02-13 18:52:17 -0500

    This is our ButtonAdapter

---
+++ Project/src/ButtonAdapter.java
@@ -0,0 +1,27 @@
+/* Definition comes from "Understanding Object Oriented Programming with
+   Java", Timothy Budd, Addison-Wesley, 1998, p213.
+   
+   Use: A simple way -- less typing -- to associate a button with an action.  Avoids a case
+   statement in actionPerformed testing which button was pressed at a cost of having many
+   anonymous classes with one instance each.
+   
+   Example:
+     westPanel.add(new ButtonAdapter("Exit") {
+       public void pressed(){ mandel.stop(); dispose();}});
+*/
+
+import java.awt.event.*;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public abstract class ButtonAdapter extends JButton implements ActionListener {
+  public ButtonAdapter (JButton button) {
+    //super (button.getName());
+    button.addActionListener(this);
+  }
+ 
+
+public void actionPerformed (ActionEvent e) { pressed(); }
+  
+  public abstract void pressed ();
+}
