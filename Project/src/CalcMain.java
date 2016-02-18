03190ccaeae82556f6e3175a16905bd0245bceba
vladimir95 <vladimirmartintsov95@gmail.com>
2016-02-18 02:21:32 -0500

    Fixed the bug with the number buttons

--- Project/src/CalcMain.java
+++ Project/src/CalcMain.java
@@ -1,7 +1,7 @@
 public class CalcMain {
 
 	public static void main(String[] args) 
-	{
+	{	//Works fine
 		//Main app in which the controller will be instantiated
 		CalcController controller = new CalcController(); 
 		/*controller.numericButton("2");
