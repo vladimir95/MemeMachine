439f74a92cb95b7a0dbae30d268a2361919fb02b
mikepod9 <mikepodimov@gmail.com>
2016-03-28 17:16:59 -0400

    Added method descriptions to GraphController class.

--- Project/src/GraphController.java
+++ Project/src/GraphController.java
@@ -13,16 +13,31 @@ public class GraphController {
 		favoritesView = new CalcFavorites(view);
 	}
 	
+	/**
+	 * Creates a new instance of DrawFunction and displays it in the main application frame.
+	 */
 	public void graph()
 	{
 		new DrawFunction(controller.getModel().getFunction(), controller.getModel().getEquation(), view, this);
 	}
 	
+	/**
+	 * Displays the Favorites View in the main application frame.
+	 * @param graph - The calling view of this function. Used to get back to the caller view.
+	 */
 	public void openFavorites(DrawFunction graph)
 	{
 		favoritesView.displayFavorites(graph);
 	}
 	
+	/**
+	 * Displays the Favorites View in the main application frame,
+	 * with the new Favorite Value with parameters functionName and yPoints, 
+	 * added to the favorites list.
+	 * @param graph - The calling view of this function. Used to get back to the caller view.
+	 * @param functionName - The String representation of the function.
+	 * @param yPoints - the y - axis data points of the function.
+	 */
 	public void addToFavourites(DrawFunction graph, String functionName, double[] yPoints)
 	{
 		favoritesView.addToFavourites(graph, functionName, yPoints);
