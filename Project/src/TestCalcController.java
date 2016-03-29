ab66334ca5fc30eb120fed9766c47d53d2255042
vince2016 <jinhuigch@hotmail.com>
2016-03-29 00:04:01 -0400

    newest controller testing cases

--- Project/src/TestCalcController.java
+++ Project/src/TestCalcController.java
@@ -23,34 +23,227 @@ public class TestCalcController
 	}
 	
 	/**
-	 * Tests that the view gets update with the proper value and history. Incorporates all operators within the equation.
+	 * Tests 
 	 */
 	@Test
-	public void testUpdateView()
+	public void testSum()
 	{
-		controller.numericButton("1");
-		controller.enter();
-		controller.numericButton("2");
-		controller.changeSign();
+		controller.numericButton("7"); // Math Error
+		controller.divide();
 		controller.sum();
-		controller.numericButton("3");
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("7");//normal input
 		controller.enter();
-		controller.numericButton("4");
+		controller.numericButton("1");
+		controller.sum();
+		assertEquals(controller.getModel().getInputValue(), "8");
+		assertEquals(controller.getHistoryText(), "7 + 1 =");
+	}
+	
+	@Test
+	public void testSubtract()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
 		controller.subtract();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("7");//normal input
+		controller.enter();
+		controller.numericButton("1");
+		controller.subtract();
+		assertEquals(controller.getModel().getInputValue(), "6");
+		assertEquals(controller.getHistoryText(), "7 - 1 =");
+	}
+	
+	@Test
+	public void testMultiply()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
 		controller.multiply();
-		controller.pi();
-		controller.numericButton("2");
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("7");//normal input
+		controller.enter();
+		controller.numericButton("1");
+		controller.multiply();
+		assertEquals(controller.getModel().getInputValue(), "7");
+		assertEquals(controller.getHistoryText(), "7 × 1 =");
+	}
+	
+	@Test
+	public void testDivide()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.divide();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("7");//normal input
+		controller.enter();
+		controller.numericButton("1");
+		controller.divide();
+		assertEquals(controller.getModel().getInputValue(), "7");
+		assertEquals(controller.getHistoryText(), "7 ÷ 1 =");
+	}
+	
+	@Test
+	public void testUndo()
+	{
+		controller.numericButton("7");
+		controller.undo();
+		assertEquals(controller.getHistoryText(), "");
+	}
+	
+	@Test
+	public void testEnter()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.enter();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("7");
+		controller.enter();
+		assertEquals(controller.getHistoryText(), "7");
+	}
+	
+	@Test
+	public void testSine()
+	{
+		controller.numericButton("7"); // Math Error
 		controller.divide();
 		controller.sine();
-		controller.pi();
-		controller.cosine();
-		controller.multiply();
-		controller.divide();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
 		controller.numericButton("0");
+		controller.sine();
+		assertEquals(controller.getHistoryText(), "sin(0) =");
+	}
+	
+	@Test
+	public void testCosine()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.cosine();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("0");
+		controller.cosine();
+		assertEquals(controller.getHistoryText(), "cos(0) =");
+	}
+	
+	@Test
+	public void testPi()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.pi();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.pi();
 		controller.enter();
+		assertEquals(controller.getHistoryText(), "π, 3.1415926536");
+	}
+	
+	@Test
+	public void testFactorial()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
 		controller.factorial();
-		controller.multiply();
-		assertEquals(controller.getDisplayText(), "-1");
-		assertEquals(controller.getHistoryText(), "((1 + -2) × (3 - 4)) ÷ (sin(π ÷ 2) × cos(π)) × 0! =");
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("3");
+		controller.factorial();
+		assertEquals(controller.getHistoryText(), "3! =");
+	}
+	
+	@Test
+	public void testChangeSign()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.changeSign();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("7");
+		controller.changeSign();
+		controller.enter();
+		assertEquals(controller.getHistoryText(), "-7");
+	}
+	
+	@Test
+	public void testNumericButton()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.numericButton("1");
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.numericButton("7");
+		controller.enter();
+		assertEquals(controller.getHistoryText(), "7");
+	}
+	
+	@Test
+	public void testGetDisplayText()
+	{
+		controller.numericButton("7"); 
+		controller.enter();
+		assertEquals(controller.getHistoryText(), controller.getDisplayText());
+	}
+	
+	@Test
+	public void testX()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.x();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.x();
+		assertEquals(controller.getHistoryText(), "x");
+	}
+	
+	@Test
+	public void testGraph()
+	{
+		controller.numericButton("7"); // Math Error
+		controller.divide();
+		controller.graph();
+		assertEquals(controller.getModel().getInputValue(), "0");
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
+		
+		controller.clear();
+		controller.graph();
+		assertEquals(controller.getHistoryText(), "Start New Calculation");
 	}
 }
