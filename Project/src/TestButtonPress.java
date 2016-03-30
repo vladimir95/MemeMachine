a8acb523328c4d375335f5907ef66352696c2a73
andrewn189 <andrewn189@gmail.com>
2016-03-30 05:41:11 -0400

    Update TestButtonPress.java

--- Project/src/TestButtonPress.java
+++ Project/src/TestButtonPress.java
@@ -1,17 +1,32 @@
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 import org.junit.*;
 
 public class TestButtonPress
 {
-	private CalcController controller;
+	private CalcFavorites favorites;
 	private CalcView view;
+	private CalcController controller;
+	private GraphController graphController;
+	private DrawFunction drawFunction;
+	private BufferedImage bi;
+	private Graphics2D g;
 	
 	@Before
 	public void setUp() throws Exception	
 	{
 		controller = new CalcController();
 		view = new CalcView(controller);
+		favorites = new CalcFavorites(view);
+		graphController = new GraphController(controller, view);
+		favorites = new CalcFavorites(view);
+		bi = null;
+		g = null;
 	}
 	
 	@Test
@@ -232,5 +247,69 @@ public class TestButtonPress
 		view.benter.doClick();
 		assertEquals(controller.getDisplayText(), "-2");
 		assertEquals(controller.getHistoryText(), "-2");
+	}
+	
+	@Test
+	public void testButtonx()
+	{
+		view.bx.doClick();
+		assertEquals(controller.getDisplayText(), "0");
+		assertEquals(controller.getHistoryText(), "x");
+	}
+	
+	@Test
+	public void testButtonSampleTest()
+	{
+		view.btest.doClick();
+		assertEquals(controller.getDisplayText(), "0");
+		assertEquals(controller.getHistoryText(), "cos(x) × x =");
+	}
+	
+	@Test
+	public void testButtonGraph()
+	{
+		drawFunction = new DrawFunction(controller.getModel().getFunction(), controller.getModel().getEquation(), view, graphController);
+		Container a = view.frame.getContentPane();
+		view.bgraph.doClick();
+		Container b = view.frame.getContentPane();
+		assertEquals(a, b);
+		
+	}
+	
+	@Test
+	public void testButtonFavouritesFromDrawFunction()
+	{
+		drawFunction = new DrawFunction(controller.getModel().getFunction(), controller.getModel().getEquation(), view, graphController);
+		drawFunction.openFavourites();
+		Container a = view.frame.getContentPane();
+		drawFunction = new DrawFunction(controller.getModel().getFunction(), controller.getModel().getEquation(), view, graphController);
+		drawFunction.bfavourites.doClick();
+		Container b = view.frame.getContentPane();
+		assertEquals(a, b);
+	}
+	
+	@Test
+	public void testButtonBackFromDrawFunction()
+	{
+		view.frame.setContentPane(view.mainpanel);
+		Container a = view.frame.getContentPane();
+		drawFunction = new DrawFunction(controller.getModel().getFunction(), controller.getModel().getEquation(), view, graphController);
+		drawFunction.bback.doClick();
+		Container b = view.frame.getContentPane();
+		assertEquals(a, b);
+	}
+	
+	@Test
+	public void testButtonAddToFavoritesFromDrawFunction()
+	{
+		controller.x();
+		drawFunction = new DrawFunction(controller.getModel().getFunction(), controller.getModel().getEquation(), view, graphController);
+		drawFunction.baddToFavourites.doClick();
+		drawFunction.openFavourites();
+		bi = new BufferedImage(view.frame.getContentPane().getWidth(), view.frame.getContentPane().getHeight(), BufferedImage.TYPE_INT_RGB);
+		g = bi.createGraphics();
+		view.frame.getContentPane().paint(g);
+		Color c = new Color(bi.getRGB(bi.getWidth()-10, 70));
+		assertEquals(c, view.functionColor);
 	}
 }
